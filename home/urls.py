f19f66298cf605463c644435bed49bec10d64ae7
miatran1109 <miatran1109@gmail.com>
2020-12-30 18:44:58 +0700

    Initial commit: base html, need to add content to each page.

---
+++ home/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # Leave as empty string for base url
+    path('', views.home, name="home"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('account/', views.account, name="account"),
+    path('wishlist/', views.wishlist, name="wishlist"),
+    path('aboutus/', views.about_us, name="about_us"),
+    path('contactus/', views.contact_us, name="contact"),
+    path('service/', views.service, name="service"),
+    path('faq/', views.faq, name="faq"),
+    path('policy/', views.policy, name="policy"),
+    path('product_list/', views.product_list, name="product_list"),
+    path('product_detail/', views.product_detail, name="product_detail"),
+    path('blog/', views.blog, name="blog"),
+    path('blog/', views.compare, name="compare"),
+]
---
+++ home/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    context = {}
+    return render(request, 'home/home.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'pages/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'pages/checkout.html', context)
+
+
+def account(request):
+    context = {}
+    return render(request, 'pages/account.html', context)
+
+
+def wishlist(request):
+    context = {}
+    return render(request, 'pages/wish_list.html', context)
+
+
+def about_us(request):
+    context = {}
+    return render(request, 'pages/about_us.html', context)
+
+
+def contact_us(request):
+    context = {}
+    return render(request, 'pages/contact_us.html', context)
+
+
+def service(request):
+    context = {}
+    return render(request, 'pages/service.html', context)
+
+
+def faq(request):
+    context = {}
+    return render(request, 'pages/faq.html', context)
+
+
+def policy(request):
+    context = {}
+    return render(request, 'pages/policy.html', context)
+
+
+def product_list(request):
+    context = {}
+    return render(request, 'pages/product_list.html', context)
+
+
+def product_detail(request):
+    context = {}
+    return render(request, 'pages/product_detail.html', context)
+
+
+def blog(request):
+    context = {}
+    return render(request, 'pages/blog.html', context)
+
+
+def compare(request):
+    context = {}
+    return render(request, 'pages/compare.html', context)
