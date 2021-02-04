4f0bcccba23ae08f6350a62209ae81bb3fcc2472
miatran1109 <59214516+miatran1109@users.noreply.github.com>
2021-02-04 10:00:09 +0700

    Create choose product model

---
+++ choose_product/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChooseProductConfig(AppConfig):
+    name = 'choose_product'
---
+++ choose_product/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class AliProduct(models.Model):
+    name = models.CharField(blank=True, max_length=50)
+    email = models.CharField(blank=True, max_length=50)
+    subject = models.CharField(blank=True, max_length=50)
+    message = models.TextField(blank=True, max_length=50)
+    ip = models.CharField(blank=True, max_length=50)
+    note = models.CharField(blank=True, max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
